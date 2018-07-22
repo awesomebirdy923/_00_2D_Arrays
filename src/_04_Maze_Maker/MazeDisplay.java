96402d44911177dce5b856f7bbb39c9e29343354
awesomebirdy923 <league@iMac-6.attlocal.net>
2018-07-22 12:55:28 -0700

    ewer
    rewrewrwerw

--- src/_04_Maze_Maker/MazeDisplay.java
+++ src/_04_Maze_Maker/MazeDisplay.java
@@ -1,44 +1,44 @@
 package _04_Maze_Maker;
+
 import java.awt.Dimension;
 import java.awt.Graphics;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class MazeDisplay extends JPanel{
+public class MazeDisplay extends JPanel {
 	private static final long serialVersionUID = 1L;
-	
+
 	public static final int WIDTH = 800;
 	public static final int HEIGHT = 800;
-	
+
 	private JFrame window;
-	
+
 	private Maze maze;
-	
-	MazeDisplay(){
+
+	MazeDisplay() {
 		super();
 		this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
-		
+
 		window = new JFrame();
-		
+
 		window.add(this);
-		
-		
-		maze = MazeMaker.generateMaze(5, 5);
-		
+
+		maze = MazeMaker.generateMaze(1, 1);
+
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setVisible(true);
 		window.pack();
 		repaint();
 	}
-	
+
 	@Override
-	public void paintComponent(Graphics g){
-		if(maze != null) {
-		maze.draw(g);
+	public void paintComponent(Graphics g) {
+		if (maze != null) {
+			maze.draw(g);
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		MazeDisplay md = new MazeDisplay();
 	}
